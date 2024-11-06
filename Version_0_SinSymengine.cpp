4420336a70112008212e26beaaf02c3c599d7cc2
Benjajamins <benjanonin8@gmail.com>
2024-11-05 22:41:01 -0300

    Add files via upload

---
+++ Version_0_SinSymengine.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<string> split(string str, char pattern) {
+  int posInit = 0;
+  int posFound = 0;
+  string splitted;
+  vector<string> results;
+
+  while ((posFound = str.find(pattern, posInit)) != string::npos) {
+      splitted = str.substr(posInit, posFound - posInit);
+      results.push_back(splitted);
+      posInit = posFound + 1;
+  }
+  if (posInit < str.size()) {
+      results.push_back(str.substr(posInit)); 
+  }
+  return results;
+}
+
+int main(){
+  string alo;
+  cin>>alo;
+  vector <string> splitted(split(alo, '+'));
+  for(int i(0);i<splitted.size(); i++){
+  cout<<splitted[i]<<endl;
+  }
+}
