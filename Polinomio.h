c7d230e716cfbd23d49fcad23cd6ffdf7c59c68d
Benjajamins <benjanonin8@gmail.com>
2024-11-05 22:37:44 -0300

    Add files via upload

---
+++ Polinomio.h
@@ -0,0 +1,56 @@
+#ifndef Polinomio_h
+#define Polinomio_h
+#include <symengine/symbol.h>
+#include <symengine/pow.h>
+#include <symengine/derivative.h>
+#include <symengine/add.h>
+#include <symengine/mul.h>
+#include <iostream>
+
+using namespace SymEngine;
+class Polinomio{
+protected:
+    RCP<const Symbol> variable; // Puntero de clase symbol que almacena la variable simbolica en este caso "x"
+    RCP<const Basic> polinomio; // Puntero de clase Basic que es una plantilla para cualquier tipo de expresion matematica basica
+public:
+    Polinomio(std::string variable); //Constructor que va a recibir el puntero variable
+    virtual RCP<const Basic> derivar();
+    virtual double evaluar(double valor);      
+    virtual void Polinomioescrito() = 0;
+};
+class Polinomiogrado2: public Polinomio{
+public:
+    Polinomiogrado2(std::string variable, int coefA, int coefB, int coefC);
+    void Polinomioescrito();
+private:
+    float a;
+    float b;
+    float c;
+};
+class Polinomiogrado3: public Polinomio{
+public:
+    Polinomiogrado3(std::string variable, float coefA, float coefB, float coefC, float coefD);
+    void Polinomioescrito();
+private:
+    float a;
+    float b;
+    float c;
+    float d;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
